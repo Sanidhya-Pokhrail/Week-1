8afe4e50e92972e2b61a9483780560a54f00b2a8
Sanidhya-Pokhrail <74345589+Sanidhya-Pokhrail@users.noreply.github.com>
2023-01-26 16:10:01 +0530

    Add files via upload

---
+++ Program9.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class Program9 {
+    public String iAmAccesibleEverywhere;
+    private String iAmAccessibleOnlyHere;
+    protected String iAmAccessibletoDerivedClasses;
+    String iAmAccessibletoAllclassesinSamePackage;
+}
